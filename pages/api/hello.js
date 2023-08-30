a2ec6a668bbe27d454ce3a97ac3223d2f796f036
Miguel Aguilar Torrez <aguilartorrezmiguel@gmail.com>
2023-08-29 22:25:11 -0400

    my first commit

---
+++ pages/api/hello.js
@@ -0,0 +1,10 @@
+import { getSession } from "next-auth/react";
+
+export default async function handler(req, res){
+
+    const session = await getSession({req})
+
+    if (!session) return res.status(403).send('Forbidden')
+
+    res.status(200).json({ name: 'John Doe'})
+}
